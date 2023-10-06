4671cd9df9def99ead576f2d0d43322a19378225
ankareta <carolinankaret@gmail.com>
2023-10-06 17:53:41 -0600

    actividad de clase

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // Escribe tu código aquí.
+console.log("Hola Mundo");
+function bigWords(inputString, stringArray) {
+    const result = [];
+  
+    for (let i = 0; i < stringArray.length; i++) {
+      if (stringArray[i].length > inputString.length) {
+        result.push(stringArray[i]);
+      }
+    }
+  
+    return result; 
+  }
